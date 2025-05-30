529419efab7368acc768b189a90035a31fe277a3
Marina Reis <marina.reis-jum@cogna.parceirosedu.com.br>
2025-05-30 19:20:36 -0300

    feat: add navbar animations

--- app/components/Navbar/index.tsx
+++ app/components/Navbar/index.tsx
@@ -1,9 +1,40 @@
+"use client"
+
+import { useGSAP } from "@gsap/react"
+import gsap from "gsap"
+import { ScrollTrigger } from "gsap/ScrollTrigger"
 import Image from "next/image"
+import { useRef, useState } from "react"
 import styles from './styles.module.scss'
 
+gsap.registerPlugin(ScrollTrigger)
+
 function Navbar() {
+  const progressRef = useRef<HTMLDivElement>(null)
+  const [hasScrolled, setHasScrolled] = useState(false)
+
+  useGSAP(() => {
+    if (!progressRef.current) return
+
+    const scrollTrigger = ScrollTrigger.create({
+      trigger: "body",
+      start: "top top",
+      end: "bottom bottom",
+      onUpdate: (self) => {
+        if (progressRef.current) {
+          progressRef.current.style.width = `${self.progress * 100}%`
+          setHasScrolled(self.progress > 0)
+        }
+      }
+    })
+
+    return () => {
+      scrollTrigger.kill()
+    }
+  }, { scope: progressRef })
+
   return (
-    <header className={styles.navbar}>
+    <header className={`${styles.navbar} ${hasScrolled ? styles.scrolled : ''}`}>
       <div className={styles.container}>
         <nav>
           <a href="/" className={styles.logo}>
@@ -17,6 +48,7 @@ function Navbar() {
           </a>
         </nav>
       </div>
+      <div className={styles.progressBar} ref={progressRef} />
     </header>
   )
 }
