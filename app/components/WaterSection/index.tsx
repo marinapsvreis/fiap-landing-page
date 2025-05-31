edefb0d297be5860acbe45f2ca4cbbc3dce973fb
Marina Reis <marina.reis-jum@cogna.parceirosedu.com.br>
2025-05-30 21:17:14 -0300

    feat: first version of water section animation

--- app/components/WaterSection/index.tsx
+++ app/components/WaterSection/index.tsx
@@ -1,9 +1,42 @@
 /* eslint-disable react/jsx-no-comment-textnodes */
+"use client";
+
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
+import { useRef } from "react";
 import styles from "./styles.module.scss";
 
+gsap.registerPlugin(ScrollTrigger);
+
 export default function WaterSection() {
+  const sectionRef = useRef<HTMLDivElement>(null);
+
+  useGSAP(() => {
+    if (!sectionRef.current) return;
+
+    gsap.fromTo(
+      sectionRef.current,
+      {
+        clipPath: "inset(0% 100% 0% 0%)",
+        opacity: 0
+      },
+      {
+        clipPath: "inset(0% 0% 0% 0%)",
+        opacity: 1,
+        ease: "power2.out",
+        scrollTrigger: {
+          trigger: sectionRef.current,
+          start: "top 80%",
+          end: "bottom -3%",
+          scrub: 1
+        }
+      }
+    );
+  }, []);
+
   return (
-    <section className={styles.water}>
+    <section className={styles.water} ref={sectionRef}>
       <p className={styles.waterDate}>04-10</p>
       <div className={styles.waterLines}>
         <div className={styles.waterLine}>
